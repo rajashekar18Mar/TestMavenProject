a677058f69349295c9a8403284c39a600d506e60
Minumula Rajashekar <minumularajashekar@gmail.com>
2019-08-17 01:26:51 +0530

    Create Java8Test.java

---
+++ Java8Test.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.Arrays;
+
+public class Java8Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List<Integer> li=Arrays.asList(1,2,3,4,9,10,5,6,7,8,1,1,1,1,1);
+		//li.forEach(System.out::println);
+		//int large=li.stream().min(Integer::compare).get();
+		li.stream().filter(i->i%2==0).forEach(System.out::print);;
+		//System.out.println(count);
+
+	}
+
+}
